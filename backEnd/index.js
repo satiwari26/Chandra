e9ad939b894fe271c73bfa0708d1fbb12730b573
satiwari26 <tiwarisaumitra26@gmail.com>
2023-06-24 22:36:05 -0700

    learned about the middleWare, understand the concept and applied the middle ware globally vs specific to a cretain route

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,7 +5,15 @@ const app = express();
 
 const PORT = 3000;  //custom port
 
- app.use(express.json());
+ app.use(express.json());   //middle ware, basically means it acts as a in-between
+ //program that is declared before the handler function(i.e (req,res)) function is declared
+ // the reason is because every time client makes the request that request first goes through the middleware method
+ //where it's data gets parsed and then handler function gets executed.
+
+ app.use((req,res,next)=>{  //global middleware that would work for any routes and globally
+    console.log(`${req.method} : ${req.url}`);
+    next(); //important to call the next middleware other wise it won't work as expected
+ });
 
 //express has these predefined methods build in, sice app is an instance of express
 //I can use it to listen the request from the clients through this port
@@ -30,7 +38,11 @@ app.listen(PORT, console.log('our app is running now!'));
     ];
 
 //get requests
-app.get('/userName', (request,response)=>{
+app.get('/userName',(request,response,next)=>{  //this is our second middleware function that has access to these parameters
+    console.log('before calling the main handler function');
+    next(); //calls the next middleware(our actual handler function)
+} 
+,(request,response)=>{
     response.send(profile);
 
 });
@@ -38,4 +50,5 @@ app.get('/userName', (request,response)=>{
 app.post('/userName',(req,res)=>{
     console.log(req.body);  //contains the data that is received from the client via req
     res.sendStatus(201);  //success code
+    profile.push(req.body);
 });
